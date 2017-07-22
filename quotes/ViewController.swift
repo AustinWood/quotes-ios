39ac101eea95f2b58f7b86cedfee50e2242438cb
Austin Wood <austin@austinkwood.com>
2017-07-22 11:04:52 -0700

    Setup test view controller

--- quotes/ViewController.swift
+++ quotes/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createNewUser()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createNewUser() {
+        print(">>> createNewUser")
+        // let urlEndpoint = "https://quotes-ios.herokuapp.com/"
     }
-
-
 }
-
