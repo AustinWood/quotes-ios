046f26e816477060495417cec4929dd444413b46
Austin Wood <austin@austinkwood.com>
2017-07-22 10:45:56 -0700

    Initial commit

---
+++ quotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quotes
+//
+//  Created by Austin Wood on 7/22/17.
+//  Copyright © 2017 Austin Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
