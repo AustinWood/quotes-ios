cfb18cf89d86c079cfd30e1ad7fa278edc15031f
Austin Wood <austin@austinkwood.com>
2017-07-22 14:30:37 -0700

    Layout SignUpVC

---
+++ quotes/SignUpVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpVC.swift
+//  quotes
+//
+//  Created by Austin Wood on 7/22/17.
+//  Copyright © 2017 Austin Wood. All rights reserved.
+//
+
+///////////////////////////////
+// TODO
+//
+// number pad for phone number
+// display number as phone number
+// hide password
+// move fields up when keyboard appears
+//
+//
+//
+///////////////////////////////
+
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
