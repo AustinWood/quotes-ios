a4c55523e26d4c98d4ca5d8d76f28bd72ac2b47f
Austin Wood <austin@austinkwood.com>
2017-07-23 10:48:03 -0700

    Create QuotesVC

---
+++ quotes/QuotesVC.swift
@@ -0,0 +1,19 @@
+//
+//  QuotesVC.swift
+//  quotes
+//
+//  Created by Austin Wood on 7/23/17.
+//  Copyright © 2017 Austin Wood. All rights reserved.
+//
+
+import UIKit
+
+class QuotesVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
