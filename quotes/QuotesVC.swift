83670ed43e8baa4087425947748976103469569b
Austin Wood <austin@austinkwood.com>
2017-07-23 12:42:28 -0700

    Filter by hear_by and/or said_by

--- quotes/QuotesVC.swift
+++ quotes/QuotesVC.swift
@@ -27,6 +27,7 @@ class QuotesVC: UIViewController {
         var paramDict: [String: Any] = [:]
         var quoteDict: [String: Any] = [:]
         quoteDict["said_by"] = 19
+        quoteDict["heard_by"] = 20
         paramDict["quote"] = quoteDict
         
         Alamofire.request(url, method: .get, parameters: paramDict)
